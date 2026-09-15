63d7e107032ebfbb8e90bc6247b4e8496b21f2f1
Suprito <a1950138@adelaide.edu.au>
2026-09-16 00:39:13 +0930

    make a script.js to record audio

---
+++ demo/src/main/resources/static/script.js
@@ -0,0 +1,49 @@
+const recordingButton = document.getElementById('recordingButton');
+const statusText = document.getElementById('status');
+
+let mediaRecorder;
+let audioChunks = [];
+let stream;
+
+let recordingStatus = false;
+
+
+recordingButton.addEventListener('click', async () =>{
+    recordingStatus = !recordingStatus;
+
+    if (recordingStatus) {
+        stream = await navigator.mediaDevices.getUserMedia({
+            audio: true
+        });
+
+        mediaRecorder = new MediaRecorder(stream);
+
+        audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event =>{
+            audioChunks.push(event.data);
+        });
+
+        mediaRecorder.start();
+
+        recordingButton.style.color = "red";
+        recordingButton.textContent = "Stop Recording";
+
+
+        statusText.style.color = "red";
+        statusText.textContent= "Recording.....";
+    } else {
+        mediaRecorder.stop();
+
+        stream.getTracks().forEach(track => {
+            track.stop();
+        });
+
+        statusText.style.color = "green";
+        statusText.textContent= "Ready";
+
+        recordingButton.style.color = "green";
+        recordingButton.textContent = "Start Recording";
+    } 
+
+});
